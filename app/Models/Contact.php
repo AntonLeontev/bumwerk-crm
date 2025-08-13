2030fb9b1bc9f87bf6d1c9f8f2c6f53b7215a86a
Anton <aner-anton@yandex.ru>
2025-08-13 20:40:23 +0300

    add: soft deleting to contacts and leads

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -7,10 +7,13 @@ use Illuminate\Database\Eloquent\Attributes\UseEloquentBuilder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[UseEloquentBuilder(ContactQueryBuilder::class)]
 class Contact extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'title',
         'name',
